10edf62eba5f9aec9e684175156c3966fa983fad
Ayo <ayoinspired@gmail.com>
2022-10-17 13:46:10 +0100

    Added more tools

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text, Image } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./styles";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { Audio } from "expo-av";
 
 const song = {
 	id: "2",
+	uri: "https://res.cloudinary.com/ayotheinspired/video/upload/v1666009895/Limoblaze-Jireh-My-Provider-Ft-Lecrae-Happi-Naijapickup.com_zbbpv5.mp3",
 	imageUri:
 		"https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg",
 	title: "Lorem Ipsum",
@@ -12,6 +14,26 @@ const song = {
 };
 
 const PlayerWidget = () => {
+	const onPlaybackStatusUpdate = (status: any) => {
+		// console.log(status);
+	};
+
+	const playCurrentSong = async () => {
+		try {
+			const sound = await Audio.Sound.createAsync(
+				{ uri: song.uri },
+				{ shouldPlay: true },
+				onPlaybackStatusUpdate
+			);
+		} catch (error) {
+			console.log("error", error);
+		}
+	};
+
+	useEffect(() => {
+		playCurrentSong();
+	}, []);
+
 	return (
 		<View style={styles.container}>
 			<Image
